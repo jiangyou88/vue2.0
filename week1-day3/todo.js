43db39c0a1624fc1f2b3196ed2057e6c2569cffb
chenjuan88 <861594815@qq.com>
2018-05-03 23:17:03 +0800

    vue

--- week1-day3/todo.js
+++ week1-day3/todo.js
@@ -1,51 +1,72 @@
-const vm=new Vue({
-    el:"#app",
+const vm = new Vue({
+    el: "#app",
     directives: {
-        focus(el,bindings){
+        focus(el, bindings) {
             //当点击当前li时让内部的输入框获取焦点
-            if(bindings.value){
-                el.focus();//获取焦点
+            if (bindings.value) {
+                el.focus(); //获取焦点
             }
         }
     },
-    data:{
-        todos:[
-            {isSelected:false,title:"睡觉"},
-            {isSelected:false,title:"吃饭"}
+    data: {
+        todos: [{
+                isSelected: false,
+                title: "睡觉"
+            },
+            {
+                isSelected: false,
+                title: "吃饭"
+            }
         ],
-        title:"",
-        cur:""
+        title: "",
+        cur: "",
+        hash: ""
     },
-    created () {//ajax获取 初始化数据
+    created() { //ajax获取 初始化数据
         //如果localStorage中有数据 就用有的 没数据 就用默认的
-        this.todos=JSON.parse(localStorage.getItem("data"))||this.todos;
+        this.todos = JSON.parse(localStorage.getItem("data")) || this.todos;
+        //监控hash值得变化,如果页面已经有hash了 重新刷新页面也要获取hash值
+        this.hash = window.location.hash.slice(2) || "all"
+        window.addEventListener("hashchange", () => {
+            //当hash值变化 重新操作记录的数据
+           this.hash=window.location.hash.slice(2);
+        }, false)
     },
     watch: {
-        todos:{//watch默认只监控一层的数据变化，深度监控
-            handler(){//默认写成函数 就相当于默认写了个handler
-                localStorage.setItem("data",JSON.stringify(this.todos));
-            },deep:true
+        todos: { //watch默认只监控一层的数据变化，深度监控
+            handler() { //默认写成函数 就相当于默认写了个handler
+                localStorage.setItem("data", JSON.stringify(this.todos));
+            },
+            deep: true
         }
     },
     computed: {
-      count(){
-          return this.todos.filter(item=>!item.isSelected).length;
-      }  
+        filterTodos(){
+            if(this.hash==="all") return this.todos;
+            if(this.hash==="finish") return this.todos.filter(item=>item.isSelected);
+            if(this.hash==="unfinish") return this.todos.filter(item=>!item.isSelected);
+        },
+        count() {
+            return this.todos.filter(item => !item.isSelected).length;
+        }
     },
     methods: {
-        add(){//keydown/keyup差一个单词，keydown的时候内容没有进入到输入框内
-            this.todos.push({isSelected:false,title:this.title});
-            this.title="";
+        add() { //keydown/keyup差一个单词，keydown的时候内容没有进入到输入框内
+            this.todos.push({
+                isSelected: false,
+                title: this.title
+            });
+            this.title = "";
         },
         //拿到当前点击的和数组里的比对相等则返回false即可
-        remove(todo){
-            this.todos=this.todos.filter(item=>item!==todo);
+        remove(todo) {
+            this.todos = this.todos.filter(item => item !== todo);
         },
-        remember(data){//当前传递的是todo (当前点击的这一项)
-            this.cur=data;
+        remember(data) { //当前传递的是todo (当前点击的这一项)
+            this.cur = data;
         },
-        cancel(){
-            this.cur="";
+        cancel() {
+            this.cur = "";
         }
     }
 });
