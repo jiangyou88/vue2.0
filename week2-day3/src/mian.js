0df84a404deff1dfd75420161ea651eb87735ae5
chenjuan88 <861594815@qq.com>
2018-05-08 23:41:24 +0800

    vue

--- week2-day3/src/mian.js
+++ week2-day3/src/mian.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 //这样直接引用vue 引用的并不是vue.js 引用的是vue的runtime
 //vue=compile+runtime(Compiler 可以编译模板)
 
+import App from "./App.vue";
+//console.log(App);
 new Vue({
-    
+    //render函数的作用是将虚拟dom渲染成真实的dom
+    //createElement返回的是虚拟的dom
+    render:(h)=> h(App)
 }).$mount("#app");
