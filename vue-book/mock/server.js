81725de73bb93b53f5698ad2c6826c6ed0de9d99
输入你的用户名 <>
2018-05-14 18:58:49 +0800

    server.js

--- vue-book/mock/server.js
+++ vue-book/mock/server.js
@@ -4,6 +4,11 @@ let url=require("url ");
 let port=3000;
 
 //获取轮播图/sliders
+let sliders=require("./sliders")
 http.createServer(function(req,res){
     let {pathname,query}=url.parse(req.url);
+    if(pathname==="/sliders"){
+        res.setHeader('Content-Type','application/json;charset=utf8');
+        res.end(JSON.stringify(sliders));
+    }
 }).listen(port);
--- vue-book/mock/sliders.js
+++ vue-book/mock/sliders.js
@@ -1,3 +1,7 @@
 module.exports=[
-    
-]
+    'https://gw.alicdn.com/imgextra/i1/116/TB21nT8iMKTBuNkSne1XXaJoXXa_!!116-0-lubanu.jpg',
+    'https://aecpm.alicdn.com/simba/img/TB1CWf9KpXXXXbuXpXXSutbFXXX.jpg_q50.jpg',
+    'https://img.alicdn.com/simba/img/TB1D02tppGWBuNjy0FbSuv4sXXa.jpg_q50.jpg',
+    'https://gw.alicdn.com/imgextra/i4/190/TB2mvqui_XYBeNkHFrdXXciuVXa_!!190-0-lubanu.jpg',
+    'https://gw.alicdn.com/imgextra/i4/131/TB2D5A8qGSWBuNjSsrbXXa0mVXa_!!131-0-lubanu.jpg'
+];
