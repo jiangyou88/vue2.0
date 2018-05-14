f7b793dcf34886f6998f4320f57301cfcad442f4
chenjuan88 <861594815@qq.com>
2018-05-14 23:53:53 +0800

    sliders

--- vue-book/src/base/Swiper.vue
+++ vue-book/src/base/Swiper.vue
@@ -1,6 +1,6 @@
 <template>
         <swiper :options="swiperOption">
-            <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">I'm Slide {{ slide }}</swiper-slide>
+            <swiper-slide v-for="(slide, index) in swiperSlides" :key="index"><img :src="slide" alt="" /></swiper-slide>
             <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
 </template>
@@ -8,14 +8,14 @@
 <script>
 export default {
   name: "carrousel",
+  props: ['swiperSlides'],
   data() {
     return {
       swiperOption: {
         pagination: {
           el: ".swiper-pagination"
         }
-      },
-      swiperSlides: [1, 2, 3, 4, 5]
+      }
     };
   }
 };
--- vue-book/src/components/Home.vue
+++ vue-book/src/components/Home.vue
@@ -2,19 +2,30 @@
   <div>
     <Header>首页</Header>
     <div class="content">
-      <Swiper></Swiper>
+      <Swiper :swiperSlides="sliders"></Swiper>
     </div>
   </div>
 </template>
 <script>
 //1.引入组件2.注册组件3.使用组件
-import Header from './Header';
-import Swiper from '../base/Swiper';
+import Header from "./Header";
+import Swiper from "../base/Swiper";
+import { getSliders } from "../api";
 export default {
+  data() {
+    return { sliders: [] };
+  },
+  async created() {
+    //给data起别名 对象中的属性名字必须和得到的结果名字一致
+    let { data: sliders } = await getSliders();
+    //将获取的数据放到sliders中
+    this.sliders = sliders;
+  },
   components: {
-    Header,Swiper
+    Header,
+    Swiper
   }
-}
+};
 </script>
 <style lang="less" scoped>
 
