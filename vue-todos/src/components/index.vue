ff1bac6ccc74cb10e57a6708d7eb8876f12293f1
chenjuan4@100tal.comm <chenjuan4@100tal.com>
2018-06-04 19:46:57 +0800

    vue-todos

---
+++ vue-todos/src/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="content">
+    <h1>{{ title }}</h1>
+    <ul>
+      <!-- 如果没有在id前面加上":"，那么Vue就会认为我们正在为id属性赋予一个字符串 -->
+      <li v-for="(todo,index) in todos" :key="index" :id="index">
+          <label>{{index+1}}.{{todo.value}}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      title: 'vue-todos',
+      todos:[
+        {value:'阅读一本关于钱的开发的书',done:false},
+        {value:'补充范例代码',done:true},
+        {value:'写心得',done:false}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../assets/site.less';
+@import '../assets/todos.less';
+
+</style>
