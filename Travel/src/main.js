a2bca62a68e5460474aa8491c274eace832b2894
chenjuan88 <861594815@qq.com>
2018-06-17 23:38:26 +0800

    header组件

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'//移动端点击click 300s点击延迟
 import '../src/assets/styles/reset.css'
 // 1px像素边框
 import '../src/assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="home">home</div>
+    <div class="home">
+        <home-header></home-header>
+    </div>
 </template>
 <script>
+import HomeHeader from '../../components/Header'
 export default {
+    name:'Home',
+    components:{
+        HomeHeader
+    }
 }
 </script>
+<style lang="stylus" scoped>
+
+</style>
