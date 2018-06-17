f3f423d2c62a62e855739b6f3cfb3f92ba40e661
chenjuan88 <861594815@qq.com>
2018-06-17 23:05:21 +0800

    css

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '../src/assets/styles/reset.css'
+// 1px像素边框
+import '../src/assets/styles/border.css'
 
 Vue.config.productionTip = false
 
