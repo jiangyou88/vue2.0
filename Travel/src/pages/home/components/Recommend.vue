dff1a3a5be19d8edc7b39b9dec07186f1240f9dd
chenjuan88 <861594815@qq.com>
2018-06-19 23:33:07 +0800

    recommend

---
+++ Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+                <img :src="item.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeRecommend',
+    data(){
+        return {
+            recommendList:[
+            {
+                id:1,
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1806/4f/4f448c94f180b36da3.water.jpg_200x200_9ef8867f.jpg',
+                title:'36kr没想到未来城',
+                desc:'来北京必去的景点非这些地方莫属'
+            },
+            {
+                id:2,
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1806/4f/4f448c94f180b36da3.water.jpg_200x200_9ef8867f.jpg',
+                title:'36kr没想到未来城',
+                desc:'来北京必去的景点非这些地方莫属'
+            },
+            {
+                id:3,
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1806/4f/4f448c94f180b36da3.water.jpg_200x200_9ef8867f.jpg',
+                title:'36kr没想到未来城',
+                desc:'来北京必去的景点非这些地方莫属'
+            }
+        ]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../../assets/styles/mixins.styl';
+.title{
+    line-height .8rem
+    background #ccc
+    text-indent .2rem
+}
+.item{
+    display flex
+    overflow hidden
+    height 1.9rem
+    img{
+        width 1.7rem
+        height 1.7rem
+        padding .1rem
+    }
+    .item-info{
+        flex 1
+        padding .1rem
+        .item-title{
+            line-height .54rem
+            font-size .32rem
+        }
+        .item-desc{
+            line-height .4rem
+            color #ccc
+            ellipsis()
+        }
+        .item-button{
+            background #ff9300
+            padding 0 .1rem
+            border-radius .06rem
+            margin-top .2rem
+            color #fff
+        }
+    }
+}
+</style>
