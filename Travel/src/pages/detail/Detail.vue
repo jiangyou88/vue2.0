08822a214e915201a82f86aa30c6ce0ff3ee7a5a
chenjuan88 <861594815@qq.com>
2018-06-24 21:53:24 +0800

    递归组件

--- Travel/src/pages/detail/Detail.vue
+++ Travel/src/pages/detail/Detail.vue
@@ -2,15 +2,30 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
+        <detail-list :list="list"></detail-list>
     </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
     name: 'Detail',
+    data() {
+        return {
+            list: [{
+                title: '成人票'
+            },{
+                title: '学生票'
+            },{
+                title: '儿童票'
+            },{
+                title: '特惠票'
+            }]
+        }
+    },
     components: {
-        DetailBanner,DetailHeader
+        DetailBanner,DetailHeader,DetailList
     }
 }
 </script>
